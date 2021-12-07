154e5a7810d4371a16b54ef9c4d43d5add0ef1bf
Italy76 <79777599+Italy76@users.noreply.github.com>
2021-12-07 00:30:15 -0300

    se agrego la funcion validarGeneral para verificar todos los campos

--- js/admin.js
+++ js/admin.js
@@ -1,10 +1,12 @@
 function campoRequerido(input){
     if(input.value.trim().length > 0){
-        console.log("paso la validacion");
+     //   console.log("paso la validacion");
         input.className = "form-control is-valid";
+        return true;
     }else{
-         console.log("no paso la validacion");
+       //  console.log("no paso la validacion");
          input.className = "form-control is-invalid";
+         return false;
     }
 }
 
@@ -15,9 +17,11 @@ function validarNumeros(input){
     if(patron.test(input.value)){
         // cumple la expresion regular
          input.className = "form-control is-valid";
+         return true;   // introducios variables booleanas en las funciones para poder insertarlas dentro del if de validacionGeneral.
     }else{
         // si no cumple a expresion regular
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
@@ -26,14 +30,26 @@ function validarURL(input){
     let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
     if(patron.test(input.value)){
         input.className = "form-control is-valid";
+        return true;
     }else{
         input.className = "form-control is-invalid";
+        return false;
     }
 }
 
 function validarGeneral(event){ // event es un objeto de la pagina web, que registra todo lo que pasa
     event.preventDefault();
     console.log("aqui tengo que validar tod de nuevo");
+    // volver a validar todos los campos
+    //if(preguntar si el codigo es correcto && pregunto si el producto es correcto)
+    let alerta = document.querySelector("#msjAlerta"); // corri aqui arriba la variable que llama el id alerta, para que sea valido dentro del if
+    if(campoRequerido(campoCodigo) && campoRequerido(campoProducto) && campoRequerido(campoDescripcion) && validarNumeros(campoCantidad) && validarURL(campoURL)){
+        console.log("si paso la validacion");
+        alerta.className = "alert alert-danger my-5 d-none";
+    }else{
+        console.log("no paso la validacion");
+        alerta.className = "alert alert-danger my-5";
+    }
 }
 // agregar eventos a los elementos del formulario
 let campoCodigo = document.querySelector("#codigo");
