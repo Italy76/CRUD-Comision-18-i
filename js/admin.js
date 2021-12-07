03d7c6b49264130aac2727c246457e410efe31c9
Italy76 <79777599+Italy76@users.noreply.github.com>
2021-12-07 03:13:02 -0300

    se creo archivo de validaciones y otro para la clase Producto; creacion de la rama crearProducto y modificacion del submit

--- js/admin.js
+++ js/admin.js
@@ -1,56 +1,5 @@
-function campoRequerido(input){
-    if(input.value.trim().length > 0){
-     //   console.log("paso la validacion");
-        input.className = "form-control is-valid";
-        return true;
-    }else{
-       //  console.log("no paso la validacion");
-         input.className = "form-control is-invalid";
-         return false;
-    }
-}
-
-function validarNumeros(input){
-    // crear una expresion regular (es como un patron)
-    let patron = /^[0-9]{1,3}$/;
-    // probar el funionamiento del patron o expresion regular
-    if(patron.test(input.value)){
-        // cumple la expresion regular
-         input.className = "form-control is-valid";
-         return true;   // introducios variables booleanas en las funciones para poder insertarlas dentro del if de validacionGeneral.
-    }else{
-        // si no cumple a expresion regular
-        input.className = "form-control is-invalid";
-        return false;
-    }
-}
-
-function validarURL(input){
-    // crear la expresion regular
-    let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
-    if(patron.test(input.value)){
-        input.className = "form-control is-valid";
-        return true;
-    }else{
-        input.className = "form-control is-invalid";
-        return false;
-    }
-}
-
-function validarGeneral(event){ // event es un objeto de la pagina web, que registra todo lo que pasa
-    event.preventDefault();
-    console.log("aqui tengo que validar tod de nuevo");
-    // volver a validar todos los campos
-    //if(preguntar si el codigo es correcto && pregunto si el producto es correcto)
-    let alerta = document.querySelector("#msjAlerta"); // corri aqui arriba la variable que llama el id alerta, para que sea valido dentro del if
-    if(campoRequerido(campoCodigo) && campoRequerido(campoProducto) && campoRequerido(campoDescripcion) && validarNumeros(campoCantidad) && validarURL(campoURL)){
-        console.log("si paso la validacion");
-        alerta.className = "alert alert-danger my-5 d-none";
-    }else{
-        console.log("no paso la validacion");
-        alerta.className = "alert alert-danger my-5";
-    }
-}
+import {campoRequerido, validarNumeros, validarURL, validarGeneral} from "./validaciones.js"
+import {Producto} from "./productoClass.js" // cada import es para uno y solo un archivo .js
 // agregar eventos a los elementos del formulario
 let campoCodigo = document.querySelector("#codigo");
 let campoProducto = document.querySelector("#producto");
@@ -64,4 +13,20 @@ campoProducto.addEventListener("blur", ()=>{campoRequerido(campoProducto)});
 campoDescripcion.addEventListener("blur", ()=>{campoRequerido(campoDescripcion)});
 campoCantidad.addEventListener("blur", ()=>{validarNumeros(campoCantidad)});
 campoURL.addEventListener("blur", ()=>{validarURL(campoURL)});
-formularioProducto.addEventListener("submit", validarGeneral);
+formularioProducto.addEventListener("submit", guardarProducto); // antes aqui habia la funcion validarGeneral. Ahora no funciona porque siendo sin parametro y habiendola trasladada a otro archivo js, aqui no funciona (las demas tienen parametro y por eso funcionan). Asi cree una nueva funcion, guardarProducto, que ahora se ocupara del submit para la creacion de un nuevo objeto.
+
+function guardarProducto(event){
+    event.preventDefault()
+    // validar los campos del formulario
+    if(validarGeneral(campoCodigo, campoProducto, campoDescripcion, campoCantidad, campoURL)){
+        // agregar o crear un producto
+        crearProducto();
+    }
+   
+}
+
+function crearProducto(){
+    console.log("aqui creo el producto");
+    // crear el objeto producto
+
+}
---
+++ js/validaciones.js
@@ -0,0 +1,56 @@
+export function campoRequerido(input){
+    if(input.value.trim().length > 0){
+     //   console.log("paso la validacion");
+        input.className = "form-control is-valid";
+        return true;
+    }else{
+       //  console.log("no paso la validacion");
+         input.className = "form-control is-invalid";
+         return false;
+    }
+}
+
+export function validarNumeros(input){
+    // crear una expresion regular (es como un patron)
+    let patron = /^[0-9]{1,3}$/;
+    // probar el funionamiento del patron o expresion regular
+    if(patron.test(input.value)){
+        // cumple la expresion regular
+         input.className = "form-control is-valid";
+         return true;   // introducios variables booleanas en las funciones para poder insertarlas dentro del if de validacionGeneral.
+    }else{
+        // si no cumple a expresion regular
+        input.className = "form-control is-invalid";
+        return false;
+    }
+}
+
+export function validarURL(input){
+    // crear la expresion regular
+    let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+    if(patron.test(input.value)){
+        input.className = "form-control is-valid";
+        return true;
+    }else{
+        input.className = "form-control is-invalid";
+        return false;
+    }
+}
+
+ export function validarGeneral(campoCodigo, campoProducto,campoDescripcion, campoCantidad, campoURL){ // Ahora inserto los parametros llamados como en el if de abajo, para que esta funcion pueda ser de tipo con parametro e pueda invocarse en en if de la funcion crearProducto de admin.js.
+    // event es un objeto de la pagina web, que registra todo lo que pasa
+   // event.preventDefault();  IMPORTANTE: saque el event y el preventDefault porque ahora el submit lo hace la funcion crearProducto en admin.js
+    console.log("aqui tengo que validar todo de nuevo");
+    // volver a validar todos los campos
+    //if(preguntar si el codigo es correcto && pregunto si el producto es correcto)
+    let alerta = document.querySelector("#msjAlerta"); // corri aqui arriba la variable que llama el id alerta, para que sea valido dentro del if
+    if(campoRequerido(campoCodigo) && campoRequerido(campoProducto) && campoRequerido(campoDescripcion) && validarNumeros(campoCantidad) && validarURL(campoURL)){
+        console.log("si paso la validacion");
+        alerta.className = "alert alert-danger my-5 d-none";
+        return true;  // introduzco valores booleanos que antes no puse, ahora porque estoy usando esta funcion dentro de un if, el if de la funcion crearProducto del archivo admin.js.
+    }else{
+        console.log("no paso la validacion");
+        alerta.className = "alert alert-danger my-5";
+        return false;
+    }
+}
